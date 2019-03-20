64f4ac95e4c95b62c6478c24e294bc010f36cf89
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-03-20 18:08:45 +0300

    Created DemoStrongName project

---
+++ NET.S.2019.Bahirau.01/ClassLibraryDemoStrongName/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassLibraryDemoStrongName
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+
+        public Product(int price, string name)
+        {
+            Price = price;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1}$", Name, Price);
+        }
+    }
+}
