651c67a6fcd33ba6d42e5259240e9d5292b06859
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-03-20 18:25:23 +0300

    Created ConsoleApplicationDemoGAC project

---
+++ NET.S.2019.Bahirau.01/ConsoleApplicationDemoGAC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibraryDemoStrongName;
+
+namespace ConsoleApplicationDemoGAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product(25, "Pink hat");
+            Console.WriteLine(product);
+            Console.ReadKey();
+        }
+    }
+}
