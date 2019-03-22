0a8c4b961d8cb3777f1a4a6f77d8ebb59c1679f9
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-03-22 15:02:18 +0300

    Added FindNextBiggerAlgo class

---
+++ NET.S.2019.Bahirau.02/NumberExtensions/FindNextBiggerAlgo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace NumberExtensions
+{
+    class FindNextBiggerAlgo
+    {
+        public int FindNextBiggerNumber(int number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            int[] digits = GetDigitsOfPositiveNumber(number);
+            digits = ReplaseDigits(digits);
+            int result = ConvertDigitsToNumber(digits);
+
+            if (result > number)
+            {
+                return result;
+            }
+
+            return -1;
+        }
+
+        private int[] ReplaseDigits(int[] digits)
+        {
+            for (int i = 0; i < digits.Length - 1; i++)
+            {
+                if (digits[i + 1] < digits[i])
+                {
+                    digits[i + 1] ^= digits[i];
+                    digits[i] ^= digits[i + 1];
+                    digits[i + 1] ^= digits[i];
+                    SortDescUntil(digits, i);
+                    return digits;
+                }
+            }
+
+            return digits;
+        }
+
+        private void SortDescUntil(int[] arr, int index)
+        {
+            for (int i = 0; i < index; i++)
+            {
+                for (int j = i + 1; j < index + 1; j++)
+                {
+                    if (arr[j] > arr[i])
+                    {
+                        arr[i] ^= arr[j];
+                        arr[j] ^= arr[i];
+                        arr[i] ^= arr[j];
+                    }
+                }
+            }
+        }
+
+        private int ConvertDigitsToNumber(int[] digits)
+        {
+            int number = 0;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                number += digits[i] * (int)Math.Pow(10, i);
+            }
+
+            return number;
+        }
+
+        private int[] GetDigitsOfPositiveNumber(int number)
+        {
+            int[] digits = new int[(int)Math.Floor(Math.Log10(number)) + 1];
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                digits[i] = number % 10;
+                number /= 10;
+            }
+
+            return digits;
+        }
+    }
+}
