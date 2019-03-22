bdf28cb60d9802631571aaf040541db3d3c434f1
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-03-22 16:26:43 +0300

    Added DigitFilter class

---
+++ NET.S.2019.Bahirau.02/NumberExtensions.Tests/DigitFilterTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NumberExtensions.Tests
+{
+    public class DigitFilterTests
+    {
+        private DigitFilter filter = new DigitFilter();
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 }, 7, new int[] { 7, 70, 17 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 68, 69, -70, 15, -17 }, 7, new int[] { 7, -70, -17 })]
+        [TestCase(new int[] { 12, 32, 84 }, 2, new int[] { 12, 32 })]
+        [TestCase(new int[] { 10, 0, 54 }, 0, new int[] { 10, 0 })]
+        [TestCase(new int[] { 94, 81 }, 3, new int[0])]
+        [TestCase(new int[0], 3, new int[0])]
+        public void FilterDigit_ArrayOfIntegers_IntegersOrEmpty(int[] numbers, 
+            int digit, int[] expected)
+        {
+            IEnumerable<int> actual = filter.FilterDigit(numbers, digit);
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestCase(-1)]
+        [TestCase(10)]
+        public void FilterDigit_NotDigitParameter_ArgumentException(int notDigit)
+        {
+            Assert.Throws<ArgumentException>(() => filter.FilterDigit(new int[] { 22, 10 }, notDigit));
+        }
+
+        [Test]
+        public void FilterDigit_NullParameter_ArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => filter.FilterDigit(null, 5));
+        }
+    }
+}
---
+++ NET.S.2019.Bahirau.02/NumberExtensions/DigitFilter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberExtensions
+{
+    public class DigitFilter
+    {
+        public IEnumerable<int> FilterDigit(IEnumerable<int> numbers, int digit)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (digit < 0 || digit > 9)
+            {
+                throw new ArgumentException();
+            }
+
+            IList<int> sortedNumbers = new List<int>();
+
+            foreach (int number in numbers)
+            {
+                if (HasDigit(number, digit))
+                {
+                    sortedNumbers.Add(number);
+                }
+            }
+
+            return sortedNumbers;
+        }
+
+        private bool HasDigit(int number, int digit)
+        {
+            number = Math.Abs(number);
+
+            if (number == digit)
+            {
+                return true;
+            }
+
+            while (number > 0)
+            {
+                if (number % 10 == digit)
+                {
+                    return true;
+                }
+
+                number /= 10;
+            }
+
+            return false;
+        }
+    }
+}
