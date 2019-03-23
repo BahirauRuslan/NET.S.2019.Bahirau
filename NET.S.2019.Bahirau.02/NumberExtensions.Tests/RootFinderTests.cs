9606c4cbef66f4bd8203eae3206227275d60845c
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-03-23 12:23:54 +0300

    Added RootFinder class

---
+++ NET.S.2019.Bahirau.02/NumberExtensions.Tests/RootFinderTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NumberExtensions.Tests
+{
+    public class RootFinderTests
+    {
+        private RootFinder rootFinder = new RootFinder();
+
+        [TestCase(0.0, 54, 0.00001, 0.0000)]
+        [TestCase(848.0, 1, 0.00001, 848.0000)]
+        [TestCase(4.0, 2, 0.001, 2.00)]
+        [TestCase(8.0, 3, 0.001, 2.00)]
+        [TestCase(-8.0, 3, 0.001, -2.00)]
+        [TestCase(0.008, 3, 0.0001, 0.200)]
+        public void FindNthRoot_ValidParameters_Result(double a, int n, double accuracy, double expected)
+        {
+            double actual = rootFinder.FindNthRoot(a, n, accuracy);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, 0.0001)]
+        [TestCase(3, -0.0001)]
+        [TestCase(-3, -0.0001)]
+        public void FindNthRoot_InvalidNandAccuracy_ArgumentException(int n, double accuracy)
+        {
+            Assert.Throws<ArgumentException>(() => rootFinder.FindNthRoot(2, n, accuracy));
+        }
+
+        [Test]
+        public void FindNthRoot_EvenNwithNugativeA_ArgumentException()
+        {
+            Assert.Throws<ArithmeticException>(() => rootFinder.FindNthRoot(-64, 6, 0.0001));
+        }
+    }
+}
---
+++ NET.S.2019.Bahirau.02/NumberExtensions/RootFinder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberExtensions
+{
+    public class RootFinder
+    {
+        public double FindNthRoot(double a, int n, double accuracy)
+        {
+            CheckParameters(a, n, accuracy);
+
+            if (a == 0 || n == 1)
+            {
+                return a;
+            }
+
+            double x = a / n;
+
+            while (Math.Abs(Math.Pow(x, n) - a) > accuracy)
+            {
+                x = 1.0 / n * ((n - 1.0) * x + a / Math.Pow(x, n - 1.0));
+            }
+
+            int digits = (accuracy < 1.0) ? (int)-Math.Floor(Math.Log10(accuracy)) - 1 : 0;
+
+            if (digits > 15)
+            {
+                digits = 15;
+            }
+
+            return Math.Round(x, digits);
+        }
+
+        private void CheckParameters(double a, int n, double accuracy)
+        {
+            if (n < 1 || accuracy <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (a < 0 && IsEven(n))
+            {
+                throw new ArithmeticException();
+            }
+        }
+
+        private bool IsEven(int num)
+        {
+            return (num & 1) == 0;
+        }
+    }
+}
