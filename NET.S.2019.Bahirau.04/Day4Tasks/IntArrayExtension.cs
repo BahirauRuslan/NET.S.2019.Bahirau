d9f02762ed914c597d1a4d1c567fc75697be58b6
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-03-30 11:08:09 +0300

    Create extension for int array

---
+++ NET.S.2019.Bahirau.04/Day4Tasks/IntArrayExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Day4Tasks
+{
+    public static class IntArrayExtension
+    {
+        public delegate int Parameter(int[] arr);
+
+        public static int ExtensionCompareTo(this int[] arr, int[] secondArr, Parameter parameter)
+        {
+            return parameter(arr).CompareTo(parameter(secondArr));
+        }
+    }
+}
