09db4d7ea3e3d46565151a94ba5aca024cc6b44f
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-03-30 16:54:50 +0300

    Add XML documentation to IntArrayExtension class

--- NET.S.2019.Bahirau.04/Day4Tasks/IntArrayExtension.cs
+++ NET.S.2019.Bahirau.04/Day4Tasks/IntArrayExtension.cs
@@ -2,10 +2,29 @@
 
 namespace Day4Tasks
 {
+    /// <summary>
+    /// Has extension method for comparing two arrays
+    /// </summary>
     public static class IntArrayExtension
     {
+        /// <summary>
+        /// Delegate to get an array property
+        /// </summary>
+        /// <param name="arr">Array of integers</param>
+        /// <returns>Integer array property</returns>
         public delegate int Parameter(int[] arr);
 
+        /// <summary>
+        /// Compares two arrays by property
+        /// </summary>
+        /// <param name="arr">Array of integers</param>
+        /// <param name="secondArr">Second array of integers</param>
+        /// <param name="parameter"> Delegate to get an array property</param>
+        /// <returns>
+        /// 1 - if property of first array more than property of second array
+        /// 0 - if property of first array equal property of second array
+        /// -1 - if property of first array less than property of second array
+        /// </returns>
         public static int ExtensionCompareTo(this int[] arr, int[] secondArr, Parameter parameter)
         {
             if (secondArr == null || parameter == null)
