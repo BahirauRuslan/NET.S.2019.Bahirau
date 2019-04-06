e3a6790aa241d400b2f389133cff02adbbcad584
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-06 19:15:58 +0300

    Add PublisherComparer

---
+++ NET.S.2019.Bahirau.08/BooksManager/PublisherComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace BooksManager
+{
+    public class PublisherComparer : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+            else if (x == null)
+            {
+                return -1;
+            }
+            else if (y == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return string.Compare(x.Publisher, y.Publisher);
+            }
+        }
+    }
+}
