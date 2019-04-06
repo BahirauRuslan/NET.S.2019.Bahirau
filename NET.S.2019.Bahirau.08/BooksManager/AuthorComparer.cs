1b5ea9453eb475039ce93fb1bbf0a1395f82782d
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-06 21:26:39 +0300

    Add xml documentation to AuthorComparer

--- NET.S.2019.Bahirau.08/BooksManager/AuthorComparer.cs
+++ NET.S.2019.Bahirau.08/BooksManager/AuthorComparer.cs
@@ -2,8 +2,17 @@
 
 namespace BooksManager
 {
+    /// <summary>
+    /// Book author comparer
+    /// </summary>
     public class AuthorComparer : IComparer<Book>
     {
+        /// <summary>
+        /// Compare two books by author name
+        /// </summary>
+        /// <param name="x">First book</param>
+        /// <param name="y">Second book</param>
+        /// <returns>-1 if x less than y, 0 if x equal y, 1 if x more than y</returns>
         public int Compare(Book x, Book y)
         {
             if (x == null && y == null)
