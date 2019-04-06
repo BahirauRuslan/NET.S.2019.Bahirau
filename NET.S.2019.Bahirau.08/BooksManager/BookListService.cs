4581c9c6965d6a93b89467cf6d5f274c7a7b0443
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-06 14:21:37 +0300

    Add BookListService abstract class

---
+++ NET.S.2019.Bahirau.08/BooksManager/BookListService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksManager
+{
+    public abstract class BookListService
+    {
+        private List<Book> _booksBuffer = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            if (Contains(book))
+            {
+                throw new ArgumentException();
+            }
+
+            _booksBuffer.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            if (!Contains(book))
+            {
+                throw new ArgumentException();
+            }
+
+            _booksBuffer.Remove(book);
+        }
+
+        public IEnumerable<Book> FindBooksByTag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SortBooksByTag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Book book)
+        {
+            return _booksBuffer.Contains(book);
+        }
+
+        public Book[] ToBookArray()
+        {
+            return _booksBuffer.ToArray();
+        }
+
+        public abstract void LoadFromStorage();
+
+        public abstract void SaveToStorage();
+    }
+}
