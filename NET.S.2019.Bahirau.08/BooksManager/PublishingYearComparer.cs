0045818e562ea661c13840fa6ae7f12a82f458ab
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-06 21:51:22 +0300

    Add xml documentation to PublishingYearComparer

--- NET.S.2019.Bahirau.08/BooksManager/PublishingYearComparer.cs
+++ NET.S.2019.Bahirau.08/BooksManager/PublishingYearComparer.cs
@@ -2,8 +2,17 @@
 
 namespace BooksManager
 {
+    /// <summary>
+    /// Book publishing year comparer
+    /// </summary>
     public class PublishingYearComparer : IComparer<Book>
     {
+        /// <summary>
+        /// Compare two books by publishing year
+        /// </summary>
+        /// <param name="x">First book</param>
+        /// <param name="y">Second book</param>
+        /// <returns>-1 if x less than y, 0 if x equal y, 1 if x more than y</returns>
         public int Compare(Book x, Book y)
         {
             if (x == null && y == null)
