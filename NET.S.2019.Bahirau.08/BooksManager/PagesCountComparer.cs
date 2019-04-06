108cace44e1e6538064c36489c248fe31f17fb59
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-06 19:21:40 +0300

    Add PagesCountComparer

---
+++ NET.S.2019.Bahirau.08/BooksManager/PagesCountComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace BooksManager
+{
+    public class PagesCountComparer : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+            else if (x == null)
+            {
+                return -1;
+            }
+            else if (y == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return x.PagesCount.CompareTo(y.PagesCount);
+            }
+        }
+    }
+}
