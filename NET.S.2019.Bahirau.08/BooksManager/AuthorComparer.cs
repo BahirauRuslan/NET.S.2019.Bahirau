11ee83c6c04b64e669b210161b9c719a1a6a8219
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-06 18:42:58 +0300

    Add AuthorComparer

---
+++ NET.S.2019.Bahirau.08/BooksManager/AuthorComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BooksManager
+{
+    public class AuthorComparer : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            return x.Author.CompareTo(y.Author);
+        }
+    }
+}
