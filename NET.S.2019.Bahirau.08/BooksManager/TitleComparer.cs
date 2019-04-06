50367dcb595c8aa9051eba5391e8442993a70e63
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-06 19:12:36 +0300

    Update TitleComparer

--- NET.S.2019.Bahirau.08/BooksManager/TitleComparer.cs
+++ NET.S.2019.Bahirau.08/BooksManager/TitleComparer.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace BooksManager
 {
-    class TitleComparer
+    public class TitleComparer : IComparer<Book>
     {
+        public int Compare(Book x, Book y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+            else if (x == null)
+            {
+                return -1;
+            }
+            else if (y == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return string.Compare(x.Title, y.Title);
+            }
+        }
     }
 }
