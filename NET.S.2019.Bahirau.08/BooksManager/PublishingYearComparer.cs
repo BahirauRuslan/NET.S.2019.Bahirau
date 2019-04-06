93cc9085e8653625a1975c2e610accbf2a383a91
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-06 19:19:06 +0300

    Add PublishingYearComparer

---
+++ NET.S.2019.Bahirau.08/BooksManager/PublishingYearComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace BooksManager
+{
+    public class PublishingYearComparer : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+            else if (x == null)
+            {
+                return -1;
+            }
+            else if (y == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return x.PublishingYear.CompareTo(y.PublishingYear);
+            }
+        }
+    }
+}
