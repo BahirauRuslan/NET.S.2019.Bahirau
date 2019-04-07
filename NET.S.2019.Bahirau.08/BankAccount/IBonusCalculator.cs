2773a575df1f3a017543a24e53b56d6ae011ec11
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-07 13:11:40 +0300

    Add IBonusCalculator interface

---
+++ NET.S.2019.Bahirau.08/BankAccount/IBonusCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace BankAccount
+{
+    /// <summary>
+    /// The IBonusCalculator interface
+    /// </summary>
+    public interface IBonusCalculator
+    {
+        /// <summary>
+        /// Get the change in bonus points
+        /// </summary>
+        /// <param name="balanceDifference">Balance difference</param>
+        /// <param name="balance">Account balance</param>
+        /// <param name="differenceCost">Cost of balance difference</param>
+        /// <param name="balanceCost">Cost of balance</param>
+        /// <returns>Decimal change in bonus points</returns>
+        decimal GetBonusDifference(decimal balanceDifference, decimal balance, int differenceCost, int balanceCost);
+    }
+}
