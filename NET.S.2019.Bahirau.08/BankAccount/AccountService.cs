1cd4a94e5c3e9dbcc0b4611aeed3a8c2c1464f81
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-07 16:37:10 +0300

    Add AccountService class

---
+++ NET.S.2019.Bahirau.08/BankAccount/AccountService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public abstract class AccountService
+    {
+        private List<Account> _buffer = new List<Account>();
+
+        public void Withdraw()
+        {
+        }
+
+        public Account[] ToAccountArray()
+        {
+            return _buffer.ToArray();
+        }
+
+        public abstract void LoadFromStorage();
+
+        public abstract void SaveToStorage();
+    }
+}
