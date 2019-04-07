0c08986a3a67697bad57e76298676ca934e22a13
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-07 17:20:11 +0300

    Add AccountBinaryFileService class

---
+++ NET.S.2019.Bahirau.08/BankAccount/AccountBinaryFileService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// The AccountBinaryFileService class.
+    /// Account service that uses a binary file as storage
+    /// </summary>
+    public class AccountBinaryFileService : AccountService
+    {
+        /// <summary>
+        /// The path to the file
+        /// </summary>
+        public string FilePath { get; set; } = "Accounts.dat";
+
+        public override void LoadFromStorage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SaveToStorage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
