22a7c3314c15599b484ffc0acdd92c1c8f10f422
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-07 16:29:37 +0300

    Add GoldAccountType class

--- NET.S.2019.Bahirau.08/BankAccount/PatinumAccountType.cs
+++ NET.S.2019.Bahirau.08/BankAccount/PatinumAccountType.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankAccount
 {
-    class PatinumAccountType
+    public class PatinumAccountType : AccountType
     {
+        public PatinumAccountType()
+        {
+            this.DifferenceCost = 21;
+            this.BalanceCost = 33;
+        }
+
+        public override string TypeName
+        {
+            get
+            {
+                return "Platinum";
+            }
+        }
+
+        public override void ChangeBonus(decimal balanceDifference, decimal balance)
+        {
+            var differenceBonus = balanceDifference * DifferenceCost;
+            var balanceBonus = balance * BalanceCost;
+            this.Bonus += (differenceBonus + balanceBonus)
+                * (Math.Abs(balanceDifference) + 11) / (Math.Abs(balance + balanceDifference) + 0.55m) * 0.01m;
+        }
     }
 }
