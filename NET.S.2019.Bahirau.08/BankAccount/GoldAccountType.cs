d31d4845e758ca22de03958ea2c4d95489648567
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-07 16:26:25 +0300

    Add GoldAccountType class

---
+++ NET.S.2019.Bahirau.08/BankAccount/GoldAccountType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BankAccount
+{
+    public class GoldAccountType : AccountType
+    {
+        public GoldAccountType()
+        {
+            this.DifferenceCost = 12;
+            this.BalanceCost = 17;
+        }
+
+        public override string TypeName
+        {
+            get
+            {
+                return "Gold";
+            }
+        }
+
+        public override void ChangeBonus(decimal balanceDifference, decimal balance)
+        {
+            var differenceBonus = balanceDifference * DifferenceCost;
+            var balanceBonus = balance * BalanceCost;
+            this.Bonus += (differenceBonus + balanceBonus)
+                * (Math.Abs(balanceDifference) + 7) / (Math.Abs(balance + balanceDifference) + 0.7m) * 0.01m;
+        }
+    }
+}
