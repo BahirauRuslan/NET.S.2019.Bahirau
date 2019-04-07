08ae9a7344028097dcca6386f8d8006db01ba5c8
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-07 16:19:46 +0300

    Add BaseAccountType class

--- NET.S.2019.Bahirau.08/BankAccount/BaseAccountType.cs
+++ NET.S.2019.Bahirau.08/BankAccount/BaseAccountType.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankAccount
 {
-    class BaseAccountType
+    public class BaseAccountType : AccountType
     {
+        public BaseAccountType()
+        {
+            this.DifferenceCost = 3;
+            this.BalanceCost = 7;
+        }
+
+        public override string TypeName
+        {
+            get
+            {
+                return "Base";
+            }
+        }
+
+        public override void ChangeBonus(decimal balanceDifference, decimal balance)
+        {
+            var differenceBonus = balanceDifference * DifferenceCost;
+            var balanceBonus = balance * BalanceCost;
+            this.Bonus += (differenceBonus + balanceBonus) 
+                * Math.Abs(balanceDifference) / (Math.Abs(balance + balanceDifference) + 1) * 0.01m;
+        }
     }
 }
