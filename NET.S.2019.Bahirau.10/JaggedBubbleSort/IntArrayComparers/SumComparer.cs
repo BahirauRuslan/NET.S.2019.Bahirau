cffa67dad96b6b3390fd6ef48db3f83452a159f5
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-13 09:56:32 +0300

    Add SumComparer to day10 tasks

---
+++ NET.S.2019.Bahirau.10/JaggedBubbleSort/IntArrayComparers/SumComparer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JaggedBubbleSort.IntArrayComparers
+{
+    public class SumComparer : IComparer<int[]>
+    {
+        public int Compare(int[] x, int[] y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+            else if (x == null)
+            {
+                return -1;
+            }
+            else if (y == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return x.Sum().CompareTo(y.Sum());
+            }
+        }
+    }
+}
