193305a14371b1e2dbcc8716897c0a3b99f3cf2d
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-13 10:50:36 +0300

    Add FirstItem comparer

---
+++ NET.S.2019.Bahirau.10/JaggedBubbleSort/IntArrayComparers/FirstItemComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace JaggedBubbleSort.IntArrayComparers
+{
+    public class FirstItemComparer : IComparer<int[]>
+    {
+        public int Compare(int[] x, int[] y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+            else if (x == null)
+            {
+                return -1;
+            }
+            else if (y == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return x[0].CompareTo(y[0]);
+            }
+        }
+    }
+}
