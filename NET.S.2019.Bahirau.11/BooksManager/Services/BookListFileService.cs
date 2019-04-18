8c40ba8904a7619cb5d5aa3c6c93544d56e2335f
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-18 12:28:38 +0300

    Add logger to BookListFileService

--- NET.S.2019.Bahirau.11/BooksManager/Services/BookListFileService.cs
+++ NET.S.2019.Bahirau.11/BooksManager/Services/BookListFileService.cs
@@ -4,22 +4,24 @@ namespace BooksManager.Services
 {
     /// <summary>
     /// The BookListFileService class.
-    /// Book service that uses a binary file as storage
+    /// Book service that uses a binary file as storage.
     /// </summary>
     public class BookListFileService : BookListService
     {
         /// <summary>
-        /// The path to the file
+        /// The path to the file.
         /// </summary>
         public string FilePath { get; set; } = "BookStorage.bklib";
 
         /// <summary>
-        /// Load books from binary file
+        /// Load books from binary file.
         /// </summary>
         public override void LoadFromStorage()
         {
+            Logger.Log(1, "Try to open or create the file " + this.FilePath);
             using (BinaryReader reader = new BinaryReader(File.Open(this.FilePath, FileMode.OpenOrCreate)))
             {
+                Logger.Log(1, "Begin reading books from file " + this.FilePath);
                 while (reader.PeekChar() > -1)
                 {
                     Book book = new Book
@@ -33,25 +35,33 @@ namespace BooksManager.Services
                     };
                     this.AddBook(book);
                 }
+
+                Logger.Log(1, "End reading books from file " + this.FilePath);
             }
         }
 
         /// <summary>
-        /// Saves books to a binary file
+        /// Saves books to a binary file.
         /// </summary>
         public override void SaveToStorage()
         {
+            Logger.Log(1, "Try to open or create the file " + this.FilePath);
             using (BinaryWriter writer = new BinaryWriter(File.Open(this.FilePath, FileMode.OpenOrCreate)))
             {
+                Logger.Log(1, "Begin writing books to file " + this.FilePath);
                 foreach (Book book in this.ToBookArray())
                 {
+                    Logger.Log(1, string.Format("Begin writing book {0} to file {1}", book, this.FilePath));
                     writer.Write(book.ISBN);
                     writer.Write(book.Author);
                     writer.Write(book.Title);
                     writer.Write(book.Publisher);
                     writer.Write(book.PublishingYear);
                     writer.Write(book.PagesCount);
+                    Logger.Log(1, string.Format("End writing book {0} to file {1}", book, this.FilePath));
                 }
+
+                Logger.Log(1, "End writing books to file " + this.FilePath);
             }
         }
     }
