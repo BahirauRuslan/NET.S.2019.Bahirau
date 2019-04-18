06904b2f55c4bc62aaf630f1d845c6344ec93051
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-18 21:04:58 +0300

    Add ConsoleUser class

---
+++ NET.S.2019.Bahirau.11/TimerEvents/ConsoleUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TimerEvents
+{
+    /// <summary>
+    /// Console user class.
+    /// </summary>
+    public class ConsoleUser
+    {
+        /// <summary>
+        /// Create ConsoleUser object.
+        /// </summary>
+        /// <param name="id">User id.</param>
+        public ConsoleUser(int id)
+        {
+            UserId = id;
+        }
+
+        /// <summary>
+        /// User id.
+        /// </summary>
+        public int UserId { get; }
+
+        /// <summary>
+        /// Print message to console.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">Timer arguments.</param>
+        public void ReceiveMessage(object sender, TimerEventArgs e)
+        {
+            Console.Write("User received a message: ");
+            Console.WriteLine(e.Message);
+            Console.WriteLine(" Sender string representation: {0}", sender);
+        }
+    }
+}
