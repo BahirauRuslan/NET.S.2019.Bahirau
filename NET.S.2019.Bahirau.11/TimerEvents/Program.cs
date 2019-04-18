f2b50e815d2458e63a882fb6f0a23a26f3cf0c96
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-18 21:33:43 +0300

    Update console UI

--- NET.S.2019.Bahirau.11/TimerEvents/Program.cs
+++ NET.S.2019.Bahirau.11/TimerEvents/Program.cs
@@ -1,15 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TimerEvents
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static int _idpeek = 1;
+        private static MyTimer _timer = new MyTimer(0);
+
+        public static void Main()
         {
+            while (MainMenu())
+            {
+            }
+        }
+        
+        private static bool MainMenu()
+        {
+            var enabled = true;
+            var menu = "1. Set timer\n2. Start timer\n" +
+                "3. Add user to timer\nCase: ";
+            var menuCase = GetInt(menu);
+            switch (menuCase)
+            {
+                case 1:
+                    SetTimer();
+                    break;
+                case 2:
+                    _timer.Start();
+                    break;
+                case 3:
+                    AddUserToTimer();
+                    break;
+                default:
+                    enabled = false;
+                    break;
+            }
+
+            return enabled;
+        }
+
+        private static void SetTimer()
+        {
+            var time = GetInt("Time delay: ");
+            _timer = new MyTimer(time);
+        }
+
+        private static void AddUserToTimer()
+        {
+            var user = new ConsoleUser(_idpeek++);
+            _timer.Beep += user.ReceiveMessage;
+            Console.WriteLine("Add user #{0}", user.UserId);
+        }
+        
+        private static int GetInt(string msg)
+        {
+            int num;
+            Console.Write(msg);
+            var strNum = Console.ReadLine();
+            while (!int.TryParse(strNum, out num))
+            {
+                Console.Write("Try again: ");
+                strNum = Console.ReadLine();
+            }
+
+            return num;
         }
     }
 }
