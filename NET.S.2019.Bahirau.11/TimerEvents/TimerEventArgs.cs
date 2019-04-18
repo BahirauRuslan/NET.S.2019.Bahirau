64e5adbe657ed1f4e72f5d2d15903914ee3fc878
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-18 20:29:03 +0300

    Add TimerEventArgs class to day11 tasks

---
+++ NET.S.2019.Bahirau.11/TimerEvents/TimerEventArgs.cs
@@ -0,0 +1,29 @@
+﻿namespace TimerEvents
+{
+    /// <summary>
+    /// TimerEventArgs class.
+    /// </summary>
+    public class TimerEventArgs
+    {
+        /// <summary>
+        /// Construct TimerEventArgs object
+        /// </summary>
+        /// <param name="message">Timer message.</param>
+        /// <param name="time">Time delay.</param>
+        public TimerEventArgs(string message, int time)
+        {
+            Message = message;
+            Time = time;
+        }
+
+        /// <summary>
+        /// Timer message.
+        /// </summary>
+        public string Message { get; }
+
+        /// <summary>
+        /// Time delay.
+        /// </summary>
+        public int Time { get; }
+    }
+}
