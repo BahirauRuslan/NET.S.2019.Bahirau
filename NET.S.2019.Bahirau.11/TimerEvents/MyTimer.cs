3f79bea5758e2883f14ffc453e674ef957605e3c
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-18 20:51:21 +0300

    Add MyTimer class to day11 tasks

---
+++ NET.S.2019.Bahirau.11/TimerEvents/MyTimer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerEvents
+{
+    /// <summary>
+    /// My timer custom class.
+    /// </summary>
+    public class MyTimer
+    {
+        /// <summary>
+        /// Time delay.
+        /// </summary>
+        private readonly int _time;
+
+        /// <summary>
+        /// Create MyTimer object.
+        /// </summary>
+        /// <param name="time">Time delay.</param>
+        public MyTimer(int time)
+        {
+            if (time < 0)
+            {
+                throw new ArgumentOutOfRangeException("Time delay must be more than zero");
+            }
+
+            _time = time;
+        }
+
+        /// <summary>
+        /// TimerState delegate.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">Timer arguments.</param>
+        public delegate void TimerState(object sender, TimerEventArgs e);
+
+        /// <summary>
+        /// Beep event.
+        /// </summary>
+        public event TimerState Beep;
+
+        /// <summary>
+        /// Start my timer.
+        /// </summary>
+        public void Start()
+        {
+            Thread.Sleep(_time);
+            Beep?.Invoke(this, new TimerEventArgs($"{_time} ms have passed", _time));
+        }
+    }
+}
