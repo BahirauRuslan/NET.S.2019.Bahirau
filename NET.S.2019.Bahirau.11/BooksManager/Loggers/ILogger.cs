9d258ae5d9e44f37f4094a9f25901d96aa714ac9
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-18 11:25:40 +0300

    Add ILogger interface

---
+++ NET.S.2019.Bahirau.11/BooksManager/Loggers/ILogger.cs
@@ -0,0 +1,15 @@
+﻿namespace BooksManager.Loggers
+{
+    /// <summary>
+    /// Logger with only one log method.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Writes the diagnostic message at the specified level.
+        /// </summary>
+        /// <param name="ordinal">Ordinal of the log level.</param>
+        /// <param name="message">Log message.</param>
+        void Log(int ordinal, string message);
+    }
+}
