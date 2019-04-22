fdb3b508a9b1b0879b84f61a27d980383266fbf1
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-22 16:34:21 +0300

    Add new solution for day13 tasks

---
+++ NET.S.2019.Bahirau.13/CustomQueue/MyQueue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomQueue
+{
+    public class MyQueue<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            Queue<int> t = new Queue<int>();
+            t.Enqueue(12);
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
