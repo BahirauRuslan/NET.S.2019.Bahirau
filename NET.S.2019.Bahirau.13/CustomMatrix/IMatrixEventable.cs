02f1692ca287beaacbfa5e9ec8b52585d55d76e0
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-24 17:42:46 +0300

    Add IMatrixEventable interface

---
+++ NET.S.2019.Bahirau.13/CustomMatrix/IMatrixEventable.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomMatrix
+{
+    /// <summary>
+    /// Change item state delegate.
+    /// </summary>
+    /// <typeparam name="T">Type of matrix items.</typeparam>
+    /// <param name="sender">Object sender.</param>
+    /// <param name="e">Matrix event arguments.</param>
+    public delegate void ChangeItemState<T>(object sender, MatrixEventArgs<T> e);
+
+    /// <summary>
+    /// Interface that represent changeState event.
+    /// </summary>
+    /// <typeparam name="T">Type of matrix items.</typeparam>
+    public interface IMatrixEventable<T>
+    {
+        /// <summary>
+        /// Change item state event.
+        /// </summary>
+        event ChangeItemState<T> ChangeState;
+    }
+}
