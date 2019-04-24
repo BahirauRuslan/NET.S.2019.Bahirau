30ed0ac3288386484cee473b3f4b46609d56021c
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-24 16:46:00 +0300

    Add CustomMatrix project for day13 tasks

---
+++ NET.S.2019.Bahirau.13/CustomMatrix/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomMatrix
+{
+    public class Class1
+    {
+    }
+}
