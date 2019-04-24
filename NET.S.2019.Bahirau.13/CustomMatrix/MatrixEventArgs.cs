6130427a058420c33f66b0a5e6c636d30b17c8bc
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-24 17:32:45 +0300

    Add MatrixEventArgs class

---
+++ NET.S.2019.Bahirau.13/CustomMatrix/MatrixEventArgs.cs
@@ -0,0 +1,44 @@
+﻿namespace CustomMatrix
+{
+    /// <summary>
+    /// Event arguments.
+    /// </summary>
+    /// <typeparam name="T">Type of matrix items.</typeparam>
+    public class MatrixEventArgs<T>
+    {
+        /// <summary>
+        /// Construct MatrixEventArgs
+        /// </summary>
+        /// <param name="row">Row index of item.</param>
+        /// <param name="column">Column index of item.</param>
+        /// <param name="before">Item state before.</param>
+        /// <param name="after">Item state after.</param>
+        public MatrixEventArgs(int row, int column, T before, T after)
+        {
+            Row = row;
+            Column = column;
+            Before = before;
+            After = after;
+        }
+
+        /// <summary>
+        /// Row index of item.
+        /// </summary>
+        public int Row { get; }
+
+        /// <summary>
+        /// Column index of item.
+        /// </summary>
+        public int Column { get; }
+
+        /// <summary>
+        /// Item state before.
+        /// </summary>
+        public T Before { get; }
+
+        /// <summary>
+        /// Item state after.
+        /// </summary>
+        public T After { get; }
+    }
+}
