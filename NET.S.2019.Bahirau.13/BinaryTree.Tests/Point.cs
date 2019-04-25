bbf89bb6796701e819e07c140c2b48abf9aaab25
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-25 11:28:49 +0300

    Add Point class

---
+++ NET.S.2019.Bahirau.13/BinaryTree.Tests/Point.cs
@@ -0,0 +1,16 @@
+﻿namespace BinaryTree.Tests
+{
+    public struct Point
+    {
+        public double X;
+        public double Y;
+        public double Z;
+
+        public Point(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
