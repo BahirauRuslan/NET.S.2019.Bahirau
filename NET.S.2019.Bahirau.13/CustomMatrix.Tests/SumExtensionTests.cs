c7a2b6b7d670ea32d1ee2a8133a9cd2fcf3d2bc8
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-25 08:43:40 +0300

    Add tests to sumExtension

---
+++ NET.S.2019.Bahirau.13/CustomMatrix.Tests/SumExtensionTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace CustomMatrix.Tests
+{
+    public class SumExtensionTests
+    {
+        private static readonly string[,] Rectangle =
+        {
+            { "12", "24", "45", "83" },
+            { "82", "ab", "Cd", "nu" }
+        };
+
+        private static readonly object[] PositiveCases =
+        {
+            new object[]
+            {
+                new StaticMatrix<string>(Rectangle),
+                new StaticMatrix<string>(Rectangle),
+                new StaticMatrix<string>(new string[,]
+                {
+                    { "1212", "2424", "4545", "8383" },
+                    { "8282", "abab", "CdCd", "nunu" }
+                })
+            },
+            new object[]
+            {
+                new RectangleMatrix<string>(Rectangle),
+                new RectangleMatrix<string>(Rectangle),
+                new StaticMatrix<string>(new string[,]
+                {
+                    { "1212", "2424", "4545", "8383" },
+                    { "8282", "abab", "CdCd", "nunu" }
+                })
+            },
+            new object[]
+            {
+                new RectangleMatrix<string>(Rectangle),
+                new StaticMatrix<string>(Rectangle),
+                new StaticMatrix<string>(new string[,]
+                {
+                    { "1212", "2424", "4545", "8383" },
+                    { "8282", "abab", "CdCd", "nunu" }
+                })
+            }
+        };
+
+        [TestCaseSource(nameof(PositiveCases))]
+        public void TestSum_RectanglePlusRectangle_Rectangle(
+            StaticMatrix<string> firstMatrix, StaticMatrix<string> secondMatrix, StaticMatrix<string> expected)
+        {
+            var actual = firstMatrix.Sum(secondMatrix);
+
+            CollectionAssert.AreEqual(expected.ToRectangleArray(), actual.ToRectangleArray());
+        }
+    }
+}
