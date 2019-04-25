4780ea263f7e7522aaf399f9dcb5c77a5c9ef59f
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-25 11:27:41 +0300

    Add Node class

---
+++ NET.S.2019.Bahirau.13/BinaryTree/Node.cs
@@ -0,0 +1,37 @@
+﻿namespace BinaryTree
+{
+    /// <summary>
+    /// Node class.
+    /// </summary>
+    /// <typeparam name="T">Type of value.</typeparam>
+    internal class Node<T>
+    {
+        /// <summary>
+        /// Creates node.
+        /// </summary>
+        /// <param name="value">Value.</param>
+        /// <param name="left">Left child node.</param>
+        /// <param name="right">Right child node.</param>
+        internal Node(T value = default, Node<T> left = null, Node<T> right = null)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+
+        /// <summary>
+        /// Left child node.
+        /// </summary>
+        internal Node<T> Left { get; set; }
+
+        /// <summary>
+        /// Right child node.
+        /// </summary>
+        internal Node<T> Right { get; set; }
+
+        /// <summary>
+        /// Value.
+        /// </summary>
+        internal T Value { get; set; }
+    }
+}
