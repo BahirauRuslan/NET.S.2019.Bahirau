6ab780470fed2bcfb13d4e84f42be5ebe3c9d363
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-25 11:29:13 +0300

    Add PointComparer class

---
+++ NET.S.2019.Bahirau.13/BinaryTree.Tests/PointComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree.Tests
+{
+    public class PointComparer : IComparer<Point>
+    {
+        public int Compare(Point p1, Point p2)
+        {
+            var length1 = Math.Sqrt((p1.X * p1.X) + (p1.Y * p1.Y) + (p1.Z * p1.Z));
+            var length2 = Math.Sqrt((p2.X * p2.X) + (p2.Y * p2.Y) + (p2.Z * p2.Z));
+            return length1.CompareTo(length2);
+        }
+    }
+}
