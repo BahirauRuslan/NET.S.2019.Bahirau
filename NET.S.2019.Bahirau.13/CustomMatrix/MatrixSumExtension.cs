e6c34bb1614c5726929b6a03ccc338e1dba28ef7
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-25 08:41:13 +0300

    Add MatrixSumExtension static class

---
+++ NET.S.2019.Bahirau.13/CustomMatrix/MatrixSumExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CustomMatrix
+{
+    /// <summary>
+    /// Static MatrixSumExtension class.
+    /// </summary>
+    public static class MatrixSumExtension
+    {
+        /// <summary>
+        /// Gets sum of two static matrix
+        /// </summary>
+        /// <typeparam name="T">Type of items.</typeparam>
+        /// <param name="firstMatrix">First static matrix.</param>
+        /// <param name="secondMatrix">Second static matrix.</param>
+        /// <returns>Static matrix that represent sum of two static matrix</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when first or second matrix is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when size of two matrix are incompatible
+        /// </exception>
+        public static RectangleMatrix<T> Sum<T>(this StaticMatrix<T> firstMatrix, StaticMatrix<T> secondMatrix)
+        {
+            if (firstMatrix == null || secondMatrix == null)
+            {
+                throw new ArgumentNullException("Matrix must be not null");
+            }
+
+            if (firstMatrix.RowsCount != secondMatrix.RowsCount 
+                || firstMatrix.ColumnsCount != secondMatrix.ColumnsCount)
+            {
+                throw new ArgumentException("Incompatible sizes of two matrix");
+            }
+
+            var itemsTable = new T[firstMatrix.RowsCount, firstMatrix.ColumnsCount];
+
+            for (var i = 0; i < firstMatrix.RowsCount; i++)
+            {
+                for (var j = 0; j < firstMatrix.ColumnsCount; j++)
+                {
+                    itemsTable[i, j] = (dynamic)firstMatrix[i, j] + (dynamic)secondMatrix[i, j];
+                }
+            }
+
+            return new RectangleMatrix<T>(itemsTable);
+        }
+    }
+}
