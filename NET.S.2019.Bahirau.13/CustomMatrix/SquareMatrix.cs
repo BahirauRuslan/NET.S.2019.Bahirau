93c3a13ef34909bc6ca4bd735db9a4de4a6a18ae
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-25 08:38:41 +0300

    Add SquareMatrix class

---
+++ NET.S.2019.Bahirau.13/CustomMatrix/SquareMatrix.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CustomMatrix
+{
+    /// <summary>
+    /// SquareMatrix class.
+    /// </summary>
+    /// <typeparam name="T">Type of items.</typeparam>
+    public class SquareMatrix<T> : RectangleMatrix<T>
+    {
+        /// <summary>
+        /// Creates square matrix.
+        /// </summary>
+        public SquareMatrix() : base()
+        {
+        }
+
+        /// <summary>
+        /// Creates square matrix from itemsTable rectangle array.
+        /// </summary>
+        /// <param name="itemsTable">Rectangle array.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when itemsTable is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when itemsTable dimension (0 or 1) length less than one.
+        /// -or- Dimensions of itemsTable not equal.
+        /// </exception>
+        public SquareMatrix(T[,] itemsTable) : base(itemsTable)
+        {
+            if (itemsTable.GetLength(0) != itemsTable.GetLength(1))
+            {
+                throw new ArgumentException("ItemsTable dimensions lengths must be equal");
+            }
+        }
+
+        /// <summary>
+        /// Creates square matrix from another square matrix.
+        /// </summary>
+        /// <param name="matrix">Square matrix.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when matrix is null.
+        /// </exception>
+        public SquareMatrix(SquareMatrix<T> matrix) : base(matrix)
+        {
+        }
+    }
+}
