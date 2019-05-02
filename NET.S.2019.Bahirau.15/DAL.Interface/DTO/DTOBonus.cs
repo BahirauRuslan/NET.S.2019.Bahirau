49b4f575f4f02ba2128eff55eab91f8e545f683b
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-02 20:32:35 +0300

    Add DTOBonus class

---
+++ NET.S.2019.Bahirau.15/DAL.Interface/DTO/DTOBonus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interface.DTO
+{
+    [Serializable]
+    public class DTOBonus
+    {
+        public int BonusPoints { get; set; }
+    }
+}
