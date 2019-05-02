3295b37d016558bc2ce7ba3bf43862354950bafe
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-02 20:37:11 +0300

    Add AccountEventArgs class

---
+++ NET.S.2019.Bahirau.15/BLL.Interface/Entities/AccountEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interface.Entities
+{
+    public class AccountEventArgs
+    {
+        public AccountEventArgs(decimal balance, decimal newBalance)
+        {
+            Balance = balance;
+            NewBalance = newBalance;
+        }
+
+        public decimal Balance { get; }
+
+        public decimal NewBalance { get; }
+    }
+}
