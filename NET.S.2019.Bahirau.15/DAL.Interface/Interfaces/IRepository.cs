a3f42b00cb041c4aa5211f1d9d55ad34d7b7f783
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-02 20:34:44 +0300

    Add IRepository interface

---
+++ NET.S.2019.Bahirau.15/DAL.Interface/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interface.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> Items { get; }
+
+        void Add(T item);
+
+        void Remove(T item);
+
+        void Update(T item);
+
+        void Save();
+    }
+}
