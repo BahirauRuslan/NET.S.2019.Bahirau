180808ee52fdaf578486c87f24340a286ac944d7
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-02 20:33:28 +0300

    Add DTOAccount class

---
+++ NET.S.2019.Bahirau.15/DAL.Interface/DTO/DTOAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interface.DTO
+{
+    public class DTOAccount
+    {
+        public long Id { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public DTOHolder Holder { get; set; }
+
+        public DTOAccountType AccountType { get; set; }
+
+        public DTOBonus Bonus { get; set; }
+
+        public bool IsEnabled { get; set; }
+    }
+}
