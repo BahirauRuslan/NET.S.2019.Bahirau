1625a149655b742de68ed5c1a892349d6d49055e
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-02 20:33:58 +0300

    Add DTOHolder class

---
+++ NET.S.2019.Bahirau.15/DAL.Interface/DTO/DTOHolder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interface.DTO
+{
+    public class DTOHolder
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public IList<DTOAccount> Accounts { get; set; }
+    }
+}
