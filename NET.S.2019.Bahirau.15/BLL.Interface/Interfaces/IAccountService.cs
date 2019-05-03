a63f1729835a2d618e939efe31f80634f6198413
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-03 19:26:31 +0300

    Add IAccountService interface

---
+++ NET.S.2019.Bahirau.15/BLL.Interface/Interfaces/IAccountService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using BLL.Interface.Entities;
+
+namespace BLL.Interface.Interfaces
+{
+    public interface IAccountService
+    {
+        void AddAccount(Holder holder, AccountType type);
+
+        void RemoveAccount(long id);
+
+        void OpenAccount(long id);
+
+        void CloseAccount(long id);
+
+        Account GetAccount(long id);
+
+        IEnumerable<Account> GetAllAccounts();
+
+        void UpdateAll();
+    }
+}
