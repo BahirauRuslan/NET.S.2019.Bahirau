ade5f02f0b8c0fa5ddaef9a925542eda390e2424
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-03 22:01:25 +0300

    Add HolderIdService class

---
+++ NET.S.2019.Bahirau.15/BLL/ServiceImplementation/HolderIdService.cs
@@ -0,0 +1,14 @@
+﻿using BLL.Interface.Interfaces;
+
+namespace BLL.ServiceImplementation
+{
+    public class HolderIdService : IHolderIdService
+    {
+        public long PrimaryNumber { get; set; } = 1;
+
+        public long GenerateHolderId()
+        {
+            return PrimaryNumber++;
+        }
+    }
+}
