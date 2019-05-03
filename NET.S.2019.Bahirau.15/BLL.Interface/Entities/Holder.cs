80d2bfc201423ef13def7cc2707cef5bde8c332d
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-03 19:24:23 +0300

    Update Holder class

--- NET.S.2019.Bahirau.15/BLL.Interface/Entities/Holder.cs
+++ NET.S.2019.Bahirau.15/BLL.Interface/Entities/Holder.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL.Interface.Entities
 {
@@ -10,17 +8,17 @@ namespace BLL.Interface.Entities
     {
         private string _name;
         private string _surname;
-        private IList<Account> _accounts;
+        private IList<long> _accounts;
 
         public Holder()
         {
             Id = 0;
             _name = string.Empty;
             _surname = string.Empty;
-            _accounts = new List<Account>();
+            _accounts = new List<long>();
         }
 
-        public Holder(long id, string name, string surname, IList<Account> accounts)
+        public Holder(long id, string name, string surname, IList<long> accounts)
         {
             Id = id;
             Name = name;
@@ -56,7 +54,7 @@ namespace BLL.Interface.Entities
             }
         }
 
-        public IList<Account> Accounts
+        public IList<long> Accounts
         {
             get
             {
@@ -89,7 +87,7 @@ namespace BLL.Interface.Entities
             return other != null &&
                    _name == other._name &&
                    _surname == other._surname &&
-                   EqualityComparer<IList<Account>>.Default.Equals(_accounts, other._accounts) &&
+                   EqualityComparer<IList<long>>.Default.Equals(_accounts, other._accounts) &&
                    Id == other.Id;
         }
 
@@ -98,14 +96,22 @@ namespace BLL.Interface.Entities
             var hashCode = 1974344722;
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(_name);
             hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(_surname);
-            hashCode = (hashCode * -1521134295) + EqualityComparer<IList<Account>>.Default.GetHashCode(_accounts);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<IList<long>>.Default.GetHashCode(_accounts);
             hashCode = (hashCode * -1521134295) + Id.GetHashCode();
             return hashCode;
         }
 
         public override string ToString()
         {
-            return $"Holder #{ Id }: { _name } { _surname }";
+            var builder = new StringBuilder($"Holder #{ Id }: { _name } { _surname }");
+
+            foreach (var account in Accounts)
+            {
+                builder.Append('\n');
+                builder.Append($"Account #{ account }");
+            }
+
+            return builder.ToString();
         }
     }
 }
