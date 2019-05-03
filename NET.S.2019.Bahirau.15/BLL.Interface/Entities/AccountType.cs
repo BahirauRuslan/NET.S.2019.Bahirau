48f5da76f47ff838e8ee7ad7a17ce4f0b384f4fc
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-03 19:23:43 +0300

    Update AccountType class

--- NET.S.2019.Bahirau.15/BLL.Interface/Entities/AccountType.cs
+++ NET.S.2019.Bahirau.15/BLL.Interface/Entities/AccountType.cs
@@ -1,16 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL.Interface.Entities
 {
-    [Serializable]
-    public enum AccountType
+    public class AccountType : IEquatable<AccountType>
     {
-        Base,
-        Gold,
-        Platinum
+        private string _typeName;
+
+        public AccountType(string typeName, AccountEvent accountEvent = null)
+        {
+            TypeName = typeName;
+            AccountEvent = accountEvent;
+        }
+
+        public string TypeName
+        {
+            get
+            {
+                return _typeName;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Type name must not be null");
+                }
+
+                if (value.Trim().Length < 1)
+                {
+                    throw new ArgumentException("Type name length must be more than zero");
+                }
+
+                _typeName = value;
+            }
+        }
+
+        public AccountEvent AccountEvent { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as AccountType);
+        }
+
+        public bool Equals(AccountType other)
+        {
+            return other != null &&
+                   _typeName == other._typeName &&
+                   EqualityComparer<AccountEvent>.Default.Equals(AccountEvent, other.AccountEvent);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1253467863;
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(_typeName);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<AccountEvent>.Default.GetHashCode(AccountEvent);
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return _typeName;
+        }
     }
 }
