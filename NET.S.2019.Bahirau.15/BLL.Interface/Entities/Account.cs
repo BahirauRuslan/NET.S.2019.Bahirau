9690e3de8ca842bc2c55832abce98debaeadcc31
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-03 19:24:42 +0300

    Update Accounts class

--- NET.S.2019.Bahirau.15/BLL.Interface/Entities/Account.cs
+++ NET.S.2019.Bahirau.15/BLL.Interface/Entities/Account.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL.Interface.Entities
 {
@@ -12,6 +9,7 @@ namespace BLL.Interface.Entities
     {
         private decimal _balance;
         private Holder _holder;
+        private AccountType _accountType;
         private Bonus _bonus;
 
         public Account()
@@ -19,7 +17,7 @@ namespace BLL.Interface.Entities
             Id = 0;
             _balance = 0;
             _holder = new Holder();
-            AccountType = AccountType.Base;
+            _accountType = new AccountType("Base");
             _bonus = new Bonus();
             IsEnabled = true;
         }
@@ -66,7 +64,18 @@ namespace BLL.Interface.Entities
             }
         }
 
-        public AccountType AccountType { get; set; }
+        public AccountType AccountType
+        {
+            get
+            {
+                return _accountType;
+            }
+
+            set
+            {
+                _accountType = value ?? throw new ArgumentNullException("Account type must not be null");
+            }
+        }
 
         public Bonus Bonus
         {
@@ -96,7 +105,7 @@ namespace BLL.Interface.Entities
         public override string ToString()
         {
             return $"Account #{ Id }\n{ _holder }\nBalance: { _balance }$\n" +
-                $"Account type: { AccountType }\nIs enabled: { IsEnabled }\n{ _bonus }";
+                $"Account type: { _accountType }\nIs enabled: { IsEnabled }\n{ _bonus }";
         }
 
         public override bool Equals(object obj)
@@ -109,20 +118,20 @@ namespace BLL.Interface.Entities
             return other != null &&
                    _balance == other._balance &&
                    EqualityComparer<Holder>.Default.Equals(_holder, other._holder) &&
+                   EqualityComparer<AccountType>.Default.Equals(_accountType, other._accountType) &&
                    EqualityComparer<Bonus>.Default.Equals(_bonus, other._bonus) &&
                    Id == other.Id &&
-                   AccountType == other.AccountType &&
                    IsEnabled == other.IsEnabled;
         }
 
         public override int GetHashCode()
         {
-            var hashCode = 965994417;
+            var hashCode = -1622109082;
             hashCode = (hashCode * -1521134295) + _balance.GetHashCode();
             hashCode = (hashCode * -1521134295) + EqualityComparer<Holder>.Default.GetHashCode(_holder);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<AccountType>.Default.GetHashCode(_accountType);
             hashCode = (hashCode * -1521134295) + EqualityComparer<Bonus>.Default.GetHashCode(_bonus);
             hashCode = (hashCode * -1521134295) + Id.GetHashCode();
-            hashCode = (hashCode * -1521134295) + AccountType.GetHashCode();
             hashCode = (hashCode * -1521134295) + IsEnabled.GetHashCode();
             return hashCode;
         }
