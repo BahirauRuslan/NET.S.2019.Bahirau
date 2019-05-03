2461bd295c9e5dbde2951c82f5d5676a63866dfc
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-03 19:15:18 +0300

    Update DTOAccount class

--- NET.S.2019.Bahirau.15/DAL.Interface/DTO/DTOAccount.cs
+++ NET.S.2019.Bahirau.15/DAL.Interface/DTO/DTOAccount.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL.Interface.DTO
 {
+    [Serializable]
     public class DTOAccount
     {
         public long Id { get; set; }
