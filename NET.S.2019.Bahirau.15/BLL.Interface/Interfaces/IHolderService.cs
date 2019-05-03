4338d6feed9f2ddf993f77433c610081717839f8
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-03 19:26:06 +0300

    Add IHolderService interface

---
+++ NET.S.2019.Bahirau.15/BLL.Interface/Interfaces/IHolderService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using BLL.Interface.Entities;
+
+namespace BLL.Interface.Interfaces
+{
+    public interface IHolderService
+    {
+        void AddHolder(string name, string surname);
+
+        void RemoveHolder(long id);
+
+        Holder GetHolder(long id);
+
+        IEnumerable<Holder> GetAllHolders();
+
+        void UpdateAll();
+    }
+}
