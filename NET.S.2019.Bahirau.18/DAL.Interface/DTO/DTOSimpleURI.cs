a382e998c8c395bfbd478b22734a318107a2437f
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-05 17:42:12 +0300

    Add DTOSimpleURI

---
+++ NET.S.2019.Bahirau.18/DAL.Interface/DTO/DTOSimpleURI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Interface.DTO
+{
+    /// <summary>
+    /// DTO SimpleURI.
+    /// </summary>
+    public class DTOSimpleURI
+    {
+        /// <summary>
+        /// URI scheme.
+        /// </summary>
+        public string Scheme { get; set; }
+
+        /// <summary>
+        /// URI host.
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// URI segments.
+        /// </summary>
+        public IList<string> Segments { get; set; }
+
+        /// <summary>
+        /// URI parameters.
+        /// </summary>
+        public IDictionary<string, string> Parameters { get; set; }
+
+        /// <summary>
+        /// Gets a string representation of URI.
+        /// </summary>
+        /// <returns>String URI.</returns>
+        public override string ToString()
+        {
+            var builder = new StringBuilder($"{ Scheme }://{ Host }");
+
+            if (Segments.Count > 0)
+            {
+                foreach (var segment in Segments)
+                {
+                    builder.Append('/');
+                    builder.Append(segment);
+                }
+            }
+
+            if (Parameters.Count > 0)
+            {
+                var parameters = from key in Parameters.Keys select $"{ key }={ Parameters[key] }";
+                builder.Append('?');
+                builder.Append(string.Join("&", parameters));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
