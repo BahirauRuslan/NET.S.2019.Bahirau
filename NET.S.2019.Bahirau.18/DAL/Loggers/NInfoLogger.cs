0639f36d922d9903dfdf5bd0ad430bbd715d49e9
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-05 14:11:11 +0300

    Add NInfoLogger class

---
+++ NET.S.2019.Bahirau.18/DAL/Loggers/NInfoLogger.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+
+namespace DAL.Loggers
+{
+    /// <summary>
+    /// A logger that uses NLog for logging.
+    /// </summary>
+    public class NInfoLogger : Interface.Interfaces.ILogger
+    {
+        /// <summary>
+        /// NLog framework logger.
+        /// </summary>
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Writes the diagnostic message at the INFO level.
+        /// </summary>
+        /// <param name="message">Log message.</param>
+        public void Log(string message)
+        {
+            this.logger.Info(message);
+        }
+    }
+}
