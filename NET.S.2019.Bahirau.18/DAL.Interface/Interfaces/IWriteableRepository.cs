114bb825d207c30a366fcf86b848899b67ce8810
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-05 17:38:05 +0300

    Add IWriteableRepository interface

---
+++ NET.S.2019.Bahirau.18/DAL.Interface/Interfaces/IWriteableRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Interface.Interfaces
+{
+    /// <summary>
+    /// Repository that can read entities.
+    /// </summary>
+    /// <typeparam name="T">Type of entities.</typeparam>
+    public interface IWriteableRepository<T>
+    {
+        /// <summary>
+        /// Write all entities.
+        /// </summary>
+        /// <param name="items">Entities.</param>
+        void WriteAll(IEnumerable<T> items);
+    }
+}
