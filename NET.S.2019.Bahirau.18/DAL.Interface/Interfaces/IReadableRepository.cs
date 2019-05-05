b0dbe2864f99c5fd67540c0195b4dc75f8d6780c
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-05 17:32:55 +0300

    Add IReadableRepository interface

---
+++ NET.S.2019.Bahirau.18/DAL.Interface/Interfaces/IReadableRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Interface.Interfaces
+{
+    /// <summary>
+    /// Repository that can read entities.
+    /// </summary>
+    /// <typeparam name="T">Type of entities.</typeparam>
+    public interface IReadableRepository<T>
+    {
+        /// <summary>
+        /// Gets all entities.
+        /// </summary>
+        /// <returns>All entities.</returns>
+        IEnumerable<T> GetAll();
+    }
+}
