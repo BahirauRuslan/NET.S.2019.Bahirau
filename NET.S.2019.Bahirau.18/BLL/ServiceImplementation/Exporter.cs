a308c2edc499ce833bb93c1f8aeab3f6a1855d87
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-05 18:50:03 +0300

    Add Exporter class

---
+++ NET.S.2019.Bahirau.18/BLL/ServiceImplementation/Exporter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BLL.Interface.Interfaces;
+using BLL.Mappers;
+using DAL.Interface.DTO;
+using DAL.Interface.Interfaces;
+
+namespace BLL.ServiceImplementation
+{
+    /// <summary>
+    /// Class that represent exporter.
+    /// </summary>
+    public class Exporter : IExporter
+    {
+        /// <summary>
+        /// Creates Exporter object.
+        /// </summary>
+        /// <param name="fromRepository">Input repository.</param>
+        /// <param name="toRepository">Output repository.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when fromRepository is null. -or- toRepository is null.
+        /// </exception>
+        public Exporter(IReadableRepository<string> fromRepository, IWriteableRepository<DTOSimpleURI> toRepository)
+        {
+            FromRepository = fromRepository ?? throw new ArgumentNullException("FromRepository must not be null");
+            ToRepository = toRepository ?? throw new ArgumentNullException("ToRepository must not be null");
+        }
+
+        /// <summary>
+        /// Input repository.
+        /// </summary>
+        public IReadableRepository<string> FromRepository { get; }
+
+        /// <summary>
+        /// Output repository.
+        /// </summary>
+        public IWriteableRepository<DTOSimpleURI> ToRepository { get; }
+
+        /// <summary>
+        /// Exports all URIs.
+        /// </summary>
+        public void ExportAll()
+        {
+            var uris = FromRepository.GetAll().ToSimpleURIs();
+            ToRepository.WriteAll(uris.ToDTOSimpleURIs());
+        }
+    }
+}
