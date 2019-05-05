118df09968aefa0d187dfdd4b4fa26bc61a3ccb0
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-05 17:28:03 +0300

    Add IExporter interface

---
+++ NET.S.2019.Bahirau.18/BLL.Interface/Interfaces/IExporter.cs
@@ -0,0 +1,13 @@
+﻿namespace BLL.Interface.Interfaces
+{
+    /// <summary>
+    /// Class that represent exporter.
+    /// </summary>
+    public interface IExporter
+    {
+        /// <summary>
+        /// Exports all entities.
+        /// </summary>
+        void ExportAll();
+    }
+}
