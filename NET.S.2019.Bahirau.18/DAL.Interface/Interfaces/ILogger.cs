91ca92f00c0a3f87f807a7448806c3b317276019
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-05 13:53:41 +0300

    Add ILogger interface

---
+++ NET.S.2019.Bahirau.18/DAL.Interface/Interfaces/ILogger.cs
@@ -0,0 +1,14 @@
+﻿namespace DAL.Interface.Interfaces
+{
+    /// <summary>
+    /// Interface that represents a simple logger.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs the message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void Log(string message);
+    }
+}
