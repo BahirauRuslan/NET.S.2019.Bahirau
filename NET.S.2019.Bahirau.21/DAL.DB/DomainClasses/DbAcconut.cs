0bf04dacf69c446fc4ffa0a38dec4e88b2926cb8
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-16 21:43:25 +0300

    Add Account domain class

---
+++ NET.S.2019.Bahirau.21/DAL.DB/DomainClasses/DbAcconut.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Interface.DTO;
+
+namespace DAL.DB.DomainClasses
+{
+    public class DbAccount
+    {
+        public long Id { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public DbHolder Holder { get; set; }
+
+        public DTOAccountType AccountType { get; set; }
+
+        public DTOBonus Bonus { get; set; }
+
+        public bool IsEnabled { get; set; }
+    }
+}
