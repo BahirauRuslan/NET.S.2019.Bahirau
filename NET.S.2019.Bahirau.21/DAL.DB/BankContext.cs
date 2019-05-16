87b9741724f759853b26dd628b764b20049d35b4
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-05-16 23:32:55 +0300

    Add BankContext class

---
+++ NET.S.2019.Bahirau.21/DAL.DB/BankContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using DAL.DB.DomainClasses;
+
+namespace DAL.DB
+{
+    public class BankContext : DbContext
+    {
+        public BankContext() : base("BankDB")
+        {
+        }
+
+        public DbSet<DbAccount> Accounts { get; set; }
+
+        public DbSet<DbHolder> Holders { get; set; }
+    }
+}
