c94a75fe445526ca0427b2c69f514788f7ffc192
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-01 22:51:45 +0300

    Add NotificationDBRepository class

---
+++ NET.S.2019.Bahirau.24/DAL.DB/Repositories/NotificationDBRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interface.DTO;
+
+namespace DAL.DB.Repositories
+{
+    public class NotificationDBRepository : DBRepository<DTONotification>
+    {
+        public NotificationDBRepository(BankContext bankContext) : base(bankContext)
+        {
+        }
+
+        protected override DbSet<DTONotification> ThisSet => BankContext.Notifications;
+    }
+}
