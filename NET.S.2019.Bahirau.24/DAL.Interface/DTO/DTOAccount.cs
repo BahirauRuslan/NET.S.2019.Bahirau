adcf97b14dd35658e58fedebc48d97a1dc1a0331
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-01 22:46:48 +0300

    Update DTOAccount class

--- NET.S.2019.Bahirau.24/DAL.Interface/DTO/DTOAccount.cs
+++ NET.S.2019.Bahirau.24/DAL.Interface/DTO/DTOAccount.cs
@@ -1,20 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL.Interface.DTO
 {
     [Serializable]
     public class DTOAccount
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
+
+        public bool IsEnabled { get; set; }
 
         public decimal Balance { get; set; }
 
-        public DTOHolder Holder { get; set; }
-
         public DTOAccountType AccountType { get; set; }
 
-        public DTOBonus Bonus { get; set; }
+        public DTOHolder Holder { get; set; }
 
-        public bool IsEnabled { get; set; }
+        public DTOBonus Bonus { get; set; }
     }
 }
