59717a95cba50339dc2cfb8d5b1b89b245b41b91
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-01 22:49:48 +0300

    Update AccountDBRepository class

--- NET.S.2019.Bahirau.24/DAL.DB/Repositories/AccountDBRepository.cs
+++ NET.S.2019.Bahirau.24/DAL.DB/Repositories/AccountDBRepository.cs
@@ -1,55 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using DAL.DB.DomainClasses;
-using DAL.DB.Mappers;
 using DAL.Interface.DTO;
 using DAL.Interface.Interfaces;
 
 namespace DAL.DB.Repositories
 {
-    public class AccountDBRepository : IRepository<DTOAccount>
+    public class AccountDBRepository : DBRepository<DTOAccount>
     {
-        private readonly BankContext _bankContext;
-
-        public AccountDBRepository(BankContext bankContext)
+        public AccountDBRepository(BankContext bankContext) : base(bankContext)
         {
-            _bankContext = bankContext ?? throw new ArgumentNullException(nameof(bankContext));
         }
 
-        public IEnumerable<DTOAccount> Items => _bankContext.Accounts.ToDTOAccounts();
-
-        public void Add(DTOAccount item)
-        {
-            var account = new DbAccount
-            {
-                Id = item.Id
-            };
-            var holder = _bankContext.Holders.Find(item.Holder.Id) ?? throw new InvalidOperationException("Holder not found.");
-            account = AccountMapper.UpdateDbAccount(item, account, holder);
-            _bankContext.Accounts.Add(account);
-        }
-
-        public void Remove(DTOAccount item)
-        {
-            _bankContext.Accounts.Remove(ToDbAccount(item));
-        }
-
-        public void Save()
-        {
-            _bankContext.SaveChanges();
-        }
-
-        public void Update(DTOAccount item)
-        {
-            _bankContext.Entry(ToDbAccount(item)).State = EntityState.Modified;
-        }
-
-        private DbAccount ToDbAccount(DTOAccount item)
-        {
-            var account = _bankContext.Accounts.Find(item.Id) ?? throw new InvalidOperationException("Account not found, incorrect id!");
-            var holder = _bankContext.Holders.Find(item.Holder.Id) ?? throw new InvalidOperationException("Holder not found, incorrect id!");
-            return AccountMapper.UpdateDbAccount(item, account, holder);
-        }
+        protected override DbSet<DTOAccount> ThisSet => BankContext.Accounts;
     }
 }
