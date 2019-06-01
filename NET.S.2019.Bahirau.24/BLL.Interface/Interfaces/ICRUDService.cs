2dca40c4a82f3a94a93b3330d288c0bca0ac53cf
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-01 23:15:11 +0300

    Add CRUD service interface

---
+++ NET.S.2019.Bahirau.24/BLL.Interface/Interfaces/ICRUDService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interface.Interfaces
+{
+    public interface ICRUDService<T>
+    {
+        void Add(T item);
+
+        T Get(long id);
+
+        void Update(T item);
+
+        void Remove(T item);
+
+        IEnumerable<T> GetAll();
+    }
+}
