aa1d1776ffb8bf039c203667b4cccfd9f01357d4
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-01 22:51:21 +0300

    Add AccountTypeDBRepository class

---
+++ NET.S.2019.Bahirau.24/DAL.DB/Repositories/AccountTypeDBRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interface.DTO;
+using DAL.Interface.Interfaces;
+
+namespace DAL.DB.Repositories
+{
+    public class AccountTypeDBRepository : DBRepository<DTOAccountType>
+    {
+        public AccountTypeDBRepository(BankContext bankContext) : base(bankContext)
+        {
+        }
+
+        protected override DbSet<DTOAccountType> ThisSet => BankContext.AccountTypes;
+    }
+}
