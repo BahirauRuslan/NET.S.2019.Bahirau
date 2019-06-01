9013646b474d71822f552b818eb99652338e3a4f
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-01 22:47:57 +0300

    Add DTONotification class

---
+++ NET.S.2019.Bahirau.24/DAL.Interface/DTO/DTONotification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Interface.DTO
+{
+    [Serializable]
+    public class DTONotification
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DTOHolder Holder { get; set; }
+    }
+}
