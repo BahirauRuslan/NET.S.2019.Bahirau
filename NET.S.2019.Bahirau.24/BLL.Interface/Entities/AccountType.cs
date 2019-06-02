ebca47c09fcea1ca3404edb4db655d9bc78e384b
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-02 09:57:30 +0300

    Update AccountType class

--- NET.S.2019.Bahirau.24/BLL.Interface/Entities/AccountType.cs
+++ NET.S.2019.Bahirau.24/BLL.Interface/Entities/AccountType.cs
@@ -1,66 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BLL.Interface.Entities
 {
-    public class AccountType : IEquatable<AccountType>
+    [DisplayName("Account type")]
+    public class AccountType
     {
-        private string _typeName;
+        [Range(0, long.MaxValue)]
+        public long Id { get; set; }
 
-        public AccountType(string typeName, AccountEvent accountEvent = null)
-        {
-            TypeName = typeName;
-            AccountEvent = accountEvent;
-        }
+        [Required]
+        [DisplayName("Type name")]
+        [StringLength(35, MinimumLength = 2)]
+        public string TypeName { get; set; }
 
-        public string TypeName
-        {
-            get
-            {
-                return _typeName;
-            }
+        [Required]
+        [DisplayName("Value")]
+        [Range(1, int.MaxValue)]
+        public int TypeValue { get; set; }
 
-            set
-            {
-                if (value == null)
-                {
-                    throw new ArgumentNullException("Type name must not be null");
-                }
-
-                if (value.Trim().Length < 1)
-                {
-                    throw new ArgumentException("Type name length must be more than zero");
-                }
-
-                _typeName = value;
-            }
-        }
-
-        public AccountEvent AccountEvent { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return this.Equals(obj as AccountType);
-        }
-
-        public bool Equals(AccountType other)
-        {
-            return other != null &&
-                   _typeName == other._typeName &&
-                   EqualityComparer<AccountEvent>.Default.Equals(AccountEvent, other.AccountEvent);
-        }
-
-        public override int GetHashCode()
-        {
-            var hashCode = -1253467863;
-            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(_typeName);
-            hashCode = (hashCode * -1521134295) + EqualityComparer<AccountEvent>.Default.GetHashCode(AccountEvent);
-            return hashCode;
-        }
-
-        public override string ToString()
-        {
-            return _typeName;
-        }
+        public IList<Account> Accounts { get; set; }
     }
 }
