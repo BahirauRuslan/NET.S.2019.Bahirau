f51f851842a1cf7a75b464548cac3cfc88a6e25b
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-02 09:57:12 +0300

    Update Account class

--- NET.S.2019.Bahirau.24/BLL.Interface/Entities/Account.cs
+++ NET.S.2019.Bahirau.24/BLL.Interface/Entities/Account.cs
@@ -1,42 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BLL.Interface.Entities
 {
     public delegate void AccountEvent(Account sender, AccountEventArgs e);
 
-    public class Account : IComparable<Account>, IEquatable<Account>
+    [DisplayName("Account")]
+    public class Account
     {
         private decimal _balance;
-        private Holder _holder;
-        private AccountType _accountType;
-        private Bonus _bonus;
-
-        public Account()
-        {
-            Id = 0;
-            _balance = 0;
-            _holder = new Holder();
-            _accountType = new AccountType("Base");
-            _bonus = new Bonus();
-            IsEnabled = true;
-        }
-
-        public Account(
-            long id, decimal balance, Holder holder, AccountType accountType, Bonus bonus, bool isEnabled = true)
-        {
-            Id = id;
-            _balance = balance;
-            Holder = holder;
-            AccountType = accountType;
-            Bonus = bonus;
-            IsEnabled = isEnabled;
-        }
 
         public event AccountEvent OnTransaction;
 
+        [Range(0, long.MaxValue)]
         public long Id { get; set; }
 
+        [Required]
+        public bool IsEnabled { get; set; }
+
+        [DisplayName("Balance")]
         public decimal Balance
         {
             get
@@ -51,89 +35,15 @@ namespace BLL.Interface.Entities
             }
         }
 
-        public Holder Holder
-        {
-            get
-            {
-                return _holder;
-            }
+        [DisplayName("Account type")]
+        [Required]
+        public AccountType AccountType { get; set; }
 
-            set
-            {
-                _holder = value ?? throw new ArgumentNullException("Holder record must not be null");
-            }
-        }
+        [DisplayName("Holder")]
+        [Required]
+        public Holder Holder { get; set; }
 
-        public AccountType AccountType
-        {
-            get
-            {
-                return _accountType;
-            }
-
-            set
-            {
-                _accountType = value ?? throw new ArgumentNullException("Account type must not be null");
-            }
-        }
-
-        public Bonus Bonus
-        {
-            get
-            {
-                return _bonus;
-            }
-
-            set
-            {
-                _bonus = value ?? throw new ArgumentNullException("Bonus record must not be null");
-            }
-        }
-
-        public bool IsEnabled { get; set; }
-
-        public int CompareTo(Account other)
-        {
-            if (other == null)
-            {
-                return 1;
-            }
-
-            return _balance.CompareTo(other._balance);
-        }
-
-        public override string ToString()
-        {
-            return $"Account #{ Id }\n{ _holder }\nBalance: { _balance }$\n" +
-                $"Account type: { _accountType }\nIs enabled: { IsEnabled }\n{ _bonus }";
-        }
-
-        public override bool Equals(object obj)
-        {
-            return this.Equals(obj as Account);
-        }
-
-        public bool Equals(Account other)
-        {
-            return other != null &&
-                   _balance == other._balance &&
-                   EqualityComparer<Holder>.Default.Equals(_holder, other._holder) &&
-                   EqualityComparer<AccountType>.Default.Equals(_accountType, other._accountType) &&
-                   EqualityComparer<Bonus>.Default.Equals(_bonus, other._bonus) &&
-                   Id == other.Id &&
-                   IsEnabled == other.IsEnabled;
-        }
-
-        public override int GetHashCode()
-        {
-            var hashCode = -1622109082;
-            hashCode = (hashCode * -1521134295) + _balance.GetHashCode();
-            hashCode = (hashCode * -1521134295) + EqualityComparer<Holder>.Default.GetHashCode(_holder);
-            hashCode = (hashCode * -1521134295) + EqualityComparer<AccountType>.Default.GetHashCode(_accountType);
-            hashCode = (hashCode * -1521134295) + EqualityComparer<Bonus>.Default.GetHashCode(_bonus);
-            hashCode = (hashCode * -1521134295) + Id.GetHashCode();
-            hashCode = (hashCode * -1521134295) + IsEnabled.GetHashCode();
-            return hashCode;
-        }
+        [DisplayName("Bonus")]
+        public Bonus Bonus { get; set; }
     }
 }
