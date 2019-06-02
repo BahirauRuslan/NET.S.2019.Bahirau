189497403b8282b509821accbfb6bb5ae0204021
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-02 09:58:59 +0300

    Add BonusEventArgs class

---
+++ NET.S.2019.Bahirau.24/BLL.Interface/Entities/BonusEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interface.Entities
+{
+    public class BonusEventArgs
+    {
+        public BonusEventArgs(int bonusPoints, int newBonusPoints)
+        {
+            BonusPoints = bonusPoints;
+            NewBonusPoints = newBonusPoints;
+        }
+
+        public decimal BonusPoints { get; }
+
+        public decimal NewBonusPoints { get; }
+    }
+}
