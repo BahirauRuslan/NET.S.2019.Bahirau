d37a6f1a4932fd8dd3e6cb1de87d21f4042fe4ff
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-02 09:58:21 +0300

    Update Holder class

--- NET.S.2019.Bahirau.24/BLL.Interface/Entities/Holder.cs
+++ NET.S.2019.Bahirau.24/BLL.Interface/Entities/Holder.cs
@@ -1,117 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BLL.Interface.Entities
 {
-    public class Holder : IComparable<Holder>, IEquatable<Holder>
+    [DisplayName("Holder")]
+    public class Holder
     {
-        private string _name;
-        private string _surname;
-        private IList<long> _accounts;
-
-        public Holder()
-        {
-            Id = 0;
-            _name = string.Empty;
-            _surname = string.Empty;
-            _accounts = new List<long>();
-        }
-
-        public Holder(long id, string name, string surname, IList<long> accounts)
-        {
-            Id = id;
-            Name = name;
-            Surname = surname;
-            Accounts = accounts;
-        }
-
+        [Range(0, long.MaxValue)]
         public long Id { get; set; }
 
-        public string Name
-        {
-            get
-            {
-                return _name;
-            }
+        [Required]
+        [DisplayName("Name")]
+        [StringLength(35, MinimumLength = 2)]
+        public string Name { get; set; }
 
-            set
-            {
-                _name = value ?? throw new ArgumentNullException("Holder name must not be null");
-            }
-        }
+        [Required]
+        [DisplayName("Surname")]
+        [StringLength(35, MinimumLength = 2)]
+        public string Surname { get; set; }
 
-        public string Surname
-        {
-            get
-            {
-                return _surname;
-            }
+        public IList<Account> Accounts { get; set; }
 
-            set
-            {
-                _surname = value ?? throw new ArgumentNullException("Holder surname must not be null");
-            }
-        }
-
-        public IList<long> Accounts
-        {
-            get
-            {
-                return _accounts;
-            }
-
-            set
-            {
-                _accounts = value ?? throw new ArgumentNullException("Holder accounts list must not be null");
-            }
-        }
-
-        public int CompareTo(Holder other)
-        {
-            if (other == null)
-            {
-                return 1;
-            }
-
-            return _name.CompareTo(other._name);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return this.Equals(obj as Holder);
-        }
-
-        public bool Equals(Holder other)
-        {
-            return other != null &&
-                   _name == other._name &&
-                   _surname == other._surname &&
-                   EqualityComparer<IList<long>>.Default.Equals(_accounts, other._accounts) &&
-                   Id == other.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            var hashCode = 1974344722;
-            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(_name);
-            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(_surname);
-            hashCode = (hashCode * -1521134295) + EqualityComparer<IList<long>>.Default.GetHashCode(_accounts);
-            hashCode = (hashCode * -1521134295) + Id.GetHashCode();
-            return hashCode;
-        }
-
-        public override string ToString()
-        {
-            var builder = new StringBuilder($"Holder #{ Id }: { _name } { _surname }");
-
-            foreach (var account in Accounts)
-            {
-                builder.Append('\n');
-                builder.Append($"Account #{ account }");
-            }
-
-            return builder.ToString();
-        }
+        public IList<Notification> Notifications { get; set; }
     }
 }
