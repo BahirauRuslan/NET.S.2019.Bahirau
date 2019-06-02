4e5b1a89ba6fa23bc97a09b2c35afda3bde490f2
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-02 09:59:22 +0300

    Add Notification class

---
+++ NET.S.2019.Bahirau.24/BLL.Interface/Entities/Notification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BLL.Interface.Entities
+{
+    [DisplayName("Notification")]
+    public class Notification
+    {
+        [Range(0, long.MaxValue)]
+        public long Id { get; set; }
+
+        [DisplayName("Content")]
+        [StringLength(5000, MinimumLength = 1)]
+        public string Content { get; set; }
+
+        [Required]
+        [DisplayName("Holder")]
+        public Holder Holder { get; set; }
+    }
+}
