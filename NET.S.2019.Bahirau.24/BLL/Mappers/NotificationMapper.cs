907dd5657667b10be7e4747dcc9f5a9ad1a0fb92
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-03 15:57:32 +0300

    Add NotificationMapper static class

---
+++ NET.S.2019.Bahirau.24/BLL/Mappers/NotificationMapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using BLL.Interface.Entities;
+using DAL.Interface.DTO;
+
+namespace BLL.Mappers
+{
+    public static class NotificationMapper
+    {
+        public static DTONotification ToDTONotification(this Notification notification)
+        {
+            return Mapper.Map<DTONotification>(notification);
+        }
+
+        public static Notification ToNotification(this DTONotification dtoNotification)
+        {
+            return Mapper.Map<Notification>(dtoNotification);
+        }
+
+        public static IEnumerable<DTONotification> ToDTONotifications(this IEnumerable<Notification> notifications)
+        {
+            return Mapper.Map<IEnumerable<DTONotification>>(notifications);
+        }
+
+        public static IEnumerable<Notification> ToNotifications(this IEnumerable<DTONotification> dtoNotifications)
+        {
+            return Mapper.Map<IEnumerable<Notification>>(dtoNotifications);
+        }
+    }
+}
