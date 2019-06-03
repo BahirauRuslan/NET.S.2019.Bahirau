d9287724c90d719923502a80caa2fad9a091ef81
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-03 15:46:19 +0300

    Update AccountDBRepository class

--- NET.S.2019.Bahirau.24/DAL.DB/Repositories/AccountDBRepository.cs
+++ NET.S.2019.Bahirau.24/DAL.DB/Repositories/AccountDBRepository.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using DAL.Interface.DTO;
-using DAL.Interface.Interfaces;
 
 namespace DAL.DB.Repositories
 {
