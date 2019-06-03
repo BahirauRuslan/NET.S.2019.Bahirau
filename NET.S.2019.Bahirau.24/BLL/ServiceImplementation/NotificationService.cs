1034c06f3b33281d281e7175db316eff1c8c04f4
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-03 20:58:44 +0300

    Add NotificationService class

---
+++ NET.S.2019.Bahirau.24/BLL/ServiceImplementation/NotificationService.cs
@@ -0,0 +1,14 @@
+﻿using BLL.Interface.Entities;
+using BLL.Interface.Interfaces;
+using DAL.Interface.DTO;
+using DAL.Interface.Interfaces;
+
+namespace BLL.ServiceImplementation
+{
+    public class NotificationService : RepositoryService<Notification, DTONotification>, INotificationService
+    {
+        public NotificationService(IRepository<DTONotification> repository) : base(repository)
+        {
+        }
+    }
+}
