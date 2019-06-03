b98b2cc8a793d4b88928faa7de42436d73745950
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-03 15:56:20 +0300

    Update AccountTypeMapper static class

--- NET.S.2019.Bahirau.24/BLL/Mappers/AccountTypeMapper.cs
+++ NET.S.2019.Bahirau.24/BLL/Mappers/AccountTypeMapper.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using AutoMapper;
 using BLL.Interface.Entities;
 using DAL.Interface.DTO;
 
@@ -8,38 +7,24 @@ namespace BLL.Mappers
 {
     public static class AccountTypeMapper
     {
-        private static readonly IList<AccountType> AccountTypes
-            = new List<AccountType>
-        {
-            new AccountType(
-                "Base", 
-                (acc, e) => acc.Bonus.BonusPoints += (int)(5m * (e.NewBalance - e.Balance) / (Math.Abs(e.NewBalance) + 9m))),
-            new AccountType(
-                "Gold",
-                (acc, e) => acc.Bonus.BonusPoints += (int)(9m * (e.NewBalance - e.Balance) / (Math.Abs(e.NewBalance) + 5m))),
-            new AccountType(
-                "Platinum",
-                (acc, e) => acc.Bonus.BonusPoints += (int)(14m * (e.NewBalance - e.Balance) / (Math.Abs(e.NewBalance) + 1m))),
-        };
-
-        public static AccountType GetAccountType(string typeName)
-        {
-            return (from type in AccountTypes where type.TypeName == typeName select type).First();
-        }
-
-        public static void AddAccountType(AccountType type)
-        {
-            AccountTypes.Add(type);
-        }
-
         public static DTOAccountType ToDTOAccountType(this AccountType accountType)
         {
-            return new DTOAccountType { TypeName = accountType.TypeName };
+            return Mapper.Map<DTOAccountType>(accountType);
         }
 
         public static AccountType ToAccountType(this DTOAccountType dtoAccountType)
         {
-            return GetAccountType(dtoAccountType.TypeName);
+            return Mapper.Map<AccountType>(dtoAccountType);
+        }
+
+        public static IEnumerable<DTOAccountType> ToDTOAccountTypes(this IEnumerable<AccountType> accountTypes)
+        {
+            return Mapper.Map<IEnumerable<DTOAccountType>>(accountTypes);
+        }
+
+        public static IEnumerable<AccountType> ToAccountTypes(this IEnumerable<DTOAccountType> dtoAccountTypes)
+        {
+            return Mapper.Map<IEnumerable<AccountType>>(dtoAccountTypes);
         }
     }
 }
