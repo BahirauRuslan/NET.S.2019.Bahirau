2e2cbd55a3460029ecd5a9ff1a626adfe8445b46
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-03 15:54:12 +0300

    Update Notification class

--- NET.S.2019.Bahirau.24/BLL.Interface/Entities/Notification.cs
+++ NET.S.2019.Bahirau.24/BLL.Interface/Entities/Notification.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BLL.Interface.Entities
 {
     [DisplayName("Notification")]
-    public class Notification
+    public class Notification : IEquatable<Notification>
     {
         [Range(0, long.MaxValue)]
         public long Id { get; set; }
@@ -21,5 +18,32 @@ namespace BLL.Interface.Entities
         [Required]
         [DisplayName("Holder")]
         public Holder Holder { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Notification);
+        }
+
+        public bool Equals(Notification other)
+        {
+            return other != null &&
+                   Id == other.Id &&
+                   Content == other.Content &&
+                   EqualityComparer<Holder>.Default.Equals(Holder, other.Holder);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 2020934497;
+            hashCode = (hashCode * -1521134295) + Id.GetHashCode();
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Content);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<Holder>.Default.GetHashCode(Holder);
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"Notification (Id = { Id }, Content = { Content }, Holder = { Holder })";
+        }
     }
 }
