d7cb5d102e82cec41d59ffe0e9faf060495e828f
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-03 20:58:20 +0300

    Add AccountTypeService class

---
+++ NET.S.2019.Bahirau.24/BLL/ServiceImplementation/AccountTypeService.cs
@@ -0,0 +1,14 @@
+﻿using BLL.Interface.Entities;
+using BLL.Interface.Interfaces;
+using DAL.Interface.DTO;
+using DAL.Interface.Interfaces;
+
+namespace BLL.ServiceImplementation
+{
+    public class AccountTypeService : RepositoryService<AccountType, DTOAccountType>, IAccountTypeService
+    {
+        public AccountTypeService(IRepository<DTOAccountType> repository) : base(repository)
+        {
+        }
+    }
+}
