89ce1466a45036ba2b33ea547e3cfe9712046553
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-03 15:53:52 +0300

    Update Holder class

--- NET.S.2019.Bahirau.24/BLL.Interface/Entities/Holder.cs
+++ NET.S.2019.Bahirau.24/BLL.Interface/Entities/Holder.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BLL.Interface.Entities
 {
     [DisplayName("Holder")]
-    public class Holder
+    public class Holder : IEquatable<Holder>
     {
         [Range(0, long.MaxValue)]
         public long Id { get; set; }
@@ -25,5 +24,32 @@ namespace BLL.Interface.Entities
         public IList<Account> Accounts { get; set; }
 
         public IList<Notification> Notifications { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Holder);
+        }
+
+        public bool Equals(Holder other)
+        {
+            return other != null &&
+                   Id == other.Id &&
+                   Name == other.Name &&
+                   Surname == other.Surname;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -129878884;
+            hashCode = (hashCode * -1521134295) + Id.GetHashCode();
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Surname);
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"Holder (Id = { Id }, Name = { Name }, Surname = { Surname })";
+        }
     }
 }
