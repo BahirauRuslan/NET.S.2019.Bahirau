76fb8d08fc7ee38522bb726b34947d047fd65c46
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-03 15:53:14 +0300

    Update Bonus class

--- NET.S.2019.Bahirau.24/BLL.Interface/Entities/Bonus.cs
+++ NET.S.2019.Bahirau.24/BLL.Interface/Entities/Bonus.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace BLL.Interface.Entities
 {
     public delegate void BonusEvent(Bonus sender, BonusEventArgs e);
 
     [DisplayName("Bonus")]
-    public class Bonus
+    public class Bonus : IEquatable<Bonus>
     {
         private int _points;
 
@@ -26,6 +25,27 @@ namespace BLL.Interface.Entities
                 OnTransaction?.Invoke(this, new BonusEventArgs(_points, value));
                 _points = value;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Bonus);
+        }
+
+        public bool Equals(Bonus other)
+        {
+            return other != null &&
+                   _points == other._points;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1928246129 + _points.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Bonus (Points = { Points })";
         }
     }
 }
