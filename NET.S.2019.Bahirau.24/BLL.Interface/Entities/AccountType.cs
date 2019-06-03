a6898e9baf9f60d4f68d1bec40a8b7ceb8cd12a5
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-03 15:52:20 +0300

    Update AccountType class

--- NET.S.2019.Bahirau.24/BLL.Interface/Entities/AccountType.cs
+++ NET.S.2019.Bahirau.24/BLL.Interface/Entities/AccountType.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 namespace BLL.Interface.Entities
 {
     [DisplayName("Account type")]
-    public class AccountType
+    public class AccountType : IEquatable<AccountType>
     {
         [Range(0, long.MaxValue)]
         public long Id { get; set; }
@@ -22,5 +22,32 @@ namespace BLL.Interface.Entities
         public int TypeValue { get; set; }
 
         public IList<Account> Accounts { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as AccountType);
+        }
+
+        public bool Equals(AccountType other)
+        {
+            return other != null &&
+                   Id == other.Id &&
+                   TypeName == other.TypeName &&
+                   TypeValue == other.TypeValue;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -7176464;
+            hashCode = (hashCode * -1521134295) + Id.GetHashCode();
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(TypeName);
+            hashCode = (hashCode * -1521134295) + TypeValue.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"AccountType (Id = { Id }, TypeName = { TypeName }, TypeValue = { TypeValue })";
+        }
     }
 }
