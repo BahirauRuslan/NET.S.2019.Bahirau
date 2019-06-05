6e6f27e26f95273d6e12bf156805ea8dcab4150b
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-05 22:46:46 +0300

    Update DTOHolder class

--- NET.S.2019.Bahirau.24/DAL.Interface/DTO/DTOHolder.cs
+++ NET.S.2019.Bahirau.24/DAL.Interface/DTO/DTOHolder.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using DAL.Interface.Interfaces;
 
 namespace DAL.Interface.DTO
 {
     [Serializable]
-    public class DTOHolder
+    public class DTOHolder : IUniqueEntity
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
