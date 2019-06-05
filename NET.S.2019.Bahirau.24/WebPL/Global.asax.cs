cc76b38c71e317d91738f5452d1e1404d1a2560f
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-05 22:56:16 +0300

    Update Web.config

--- NET.S.2019.Bahirau.24/WebPL/Global.asax.cs
+++ NET.S.2019.Bahirau.24/WebPL/Global.asax.cs
@@ -5,13 +5,19 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using DependencyResolver;
+using Ninject;
 
 namespace WebPL
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static IKernel Resolver { get; } = new StandardKernel();
+
         protected void Application_Start()
         {
+            AutoMapperConfig.Configure();
+            Resolver.ConfigurateResolver();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
