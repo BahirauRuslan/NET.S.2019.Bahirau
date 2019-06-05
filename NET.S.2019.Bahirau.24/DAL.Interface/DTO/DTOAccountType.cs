29c383ef3b7777646b55dc2f5c91d143964f9678
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-06-05 22:46:31 +0300

    Update DTOAccountType class

--- NET.S.2019.Bahirau.24/DAL.Interface/DTO/DTOAccountType.cs
+++ NET.S.2019.Bahirau.24/DAL.Interface/DTO/DTOAccountType.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using DAL.Interface.Interfaces;
 
 namespace DAL.Interface.DTO
 {
     [Serializable]
-    public class DTOAccountType
+    public class DTOAccountType : IUniqueEntity
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
